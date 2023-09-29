e8ef66942c69077370afb338280f50ade97ae777
leverashes <egor.leverash@gmail.com>
2023-09-29 14:35:35 +0500

    Доброска всё 3ей домашки. Задача 3 с говнокодом

--- pythonProject/lesson_3/task2.py
+++ pythonProject/lesson_3/task2.py
@@ -3,7 +3,10 @@
 # сумму и среднее арифметическое с округлением до 2 знаков после запятой;
 
 def get_list_info(lst):
-    # Здесь пишем код
+    min_elem = min(lst)
+    max_elem = max(lst)
+    sum_list = sum(lst)
+    average = round(sum_list / len(lst), 2)
     return min_elem, max_elem, sum_list, average
 
 # Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
